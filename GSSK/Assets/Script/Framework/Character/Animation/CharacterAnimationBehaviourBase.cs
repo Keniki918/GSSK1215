ccdaeffa3a1d08ebf01019106d16a1d83427e63c
Keniki <developerkeniki@gmail.com>
2018-12-15 18:44:43 +0900

    キャラアニメーションまわり

---
+++ GSSK/Assets/Script/Framework/Character/Animation/CharacterAnimationBehaviourBase.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Framework
+{
+    /// <summary>
+    /// キャラクター単位アニメーション管理クラス
+    /// </summary>
+    public class CharacterAnimationBehaviourBase : MonoBehaviour
+    {
+        protected Animator _animator;
+        // Use this for initialization
+        void Awake()
+        {
+            // Animator取得
+            this._animator = GetComponent<Animator>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            // テストコード
+            if (Input.GetKey("up"))
+            {
+                _animator.SetBool("walk", true);
+            }
+            else
+            {
+                _animator.SetBool("walk", false);
+            }
+
+            _animator.SetBool("attack", Input.GetKey("a"));
+        }
+    }
+}
